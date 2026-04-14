21cd5222de4959e283ac28e67fb2968aac5de7a7
Almonacid98 <gabriel98almonacid@hotmail.com>
2026-04-14 20:51:37 -0300

    Cree los modelos y modifique los que tenia al inicio, borrando una tabla y aplicando solo una tabla usuario que separa los usuarios por roles tipo admin y tipo usuario, apliqué los cambios y las tablas iniciales se migraron con exito a la base de datos en postgres.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    ROL_CHOICES = (
+        ('usuario', 'Usuario'),
+        ('creador', 'Creador'),
+    )
+
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    edad = models.IntegerField()
+    genero = models.CharField(max_length=50)
+
+    rol = models.CharField(max_length=10, choices=ROL_CHOICES)
+    fecha_registro = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.rol})"
+
+
+class Contenido(models.Model):
+    titulo = models.CharField(max_length=200)
+    tipo = models.CharField(max_length=50)
+    genero = models.CharField(max_length=100)
+    anio = models.IntegerField()
+    duracion_min = models.IntegerField()
+
+    creador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
+
+class Visualizacion(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    contenido = models.ForeignKey(Contenido, on_delete=models.CASCADE)
+    fecha_visualizacion = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.usuario} vio {self.contenido}"
