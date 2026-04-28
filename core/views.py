93c1e1948bcfa90e0d84398172333cac59a630a8
Almonacid98 <gabriel98almonacid@hotmail.com>
2026-04-28 19:53:44 -0300

    cree vistas CRUD

--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Usuario, Contenido, Visualizacion
+from .serializers import UsuarioSerializer, ContenidoSerializer, VisualizacionSerializer
 
-# Create your views here.
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nombre', 'email']
+
+
+class ContenidoViewSet(viewsets.ModelViewSet):
+    queryset = Contenido.objects.all()
+    serializer_class = ContenidoSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['titulo', 'genero', 'tipo']
+
+
+class VisualizacionViewSet(viewsets.ModelViewSet):
+    queryset = Visualizacion.objects.all()
+    serializer_class = VisualizacionSerializer
