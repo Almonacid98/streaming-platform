0be337e26ad62115573bf19c4fa3459e49f9c91b
Almonacid98 <gabriel98almonacid@hotmail.com>
2026-04-28 19:36:31 -0300

    Registré modelos en el admin y los probé, las tablas existen y se puede cargar el contenido

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Usuario, Contenido, Visualizacion
 
-# Register your models here.
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre", "email", "edad", "genero", "rol", "fecha_registro")
+    search_fields = ("nombre", "email")
+    list_filter = ("rol", "genero", "fecha_registro")
+
+
+@admin.register(Contenido)
+class ContenidoAdmin(admin.ModelAdmin):
+    list_display = ("id", "titulo", "tipo", "genero", "anio", "duracion_min", "creador")
+    search_fields = ("titulo", "genero", "tipo")
+    list_filter = ("tipo", "genero", "anio")
+
+
+@admin.register(Visualizacion)
+class VisualizacionAdmin(admin.ModelAdmin):
+    list_display = ("id", "usuario", "contenido", "fecha_visualizacion")
+    search_fields = ("usuario__nombre", "contenido__titulo")
+    list_filter = ("fecha_visualizacion",)
