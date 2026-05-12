38852e2b406e9717751474a6c2467f66a68423a1
Almonacid98 <gabriel98almonacid@hotmail.com>
2026-05-12 16:14:35 -0300

    Se actualizaron los serializers sin user solo contenido y visualización

--- core/serializers.py
+++ core/serializers.py
@@ -1,28 +1,43 @@
 from rest_framework import serializers
-from .models import Usuario, Contenido, Visualizacion
+
+from .models import Contenido, Visualizacion
+from users.models import User
 
 
-class UsuarioSerializer(serializers.ModelSerializer):
+class UserBasicSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Usuario
-        fields = '__all__'
+        model = User
+        fields = ['id', 'username', 'email', 'rol']
 
 
 class ContenidoSerializer(serializers.ModelSerializer):
+    creador = UserBasicSerializer(read_only=True)
+
+    creador_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        source='creador',
+        write_only=True
+    )
+
     class Meta:
         model = Contenido
         fields = '__all__'
 
 
 class VisualizacionSerializer(serializers.ModelSerializer):
-    usuario = UsuarioSerializer(read_only=True)
+    usuario = UserBasicSerializer(read_only=True)
     contenido = ContenidoSerializer(read_only=True)
 
     usuario_id = serializers.PrimaryKeyRelatedField(
-        queryset=Usuario.objects.all(), source='usuario', write_only=True
+        queryset=User.objects.all(),
+        source='usuario',
+        write_only=True
     )
+
     contenido_id = serializers.PrimaryKeyRelatedField(
-        queryset=Contenido.objects.all(), source='contenido', write_only=True
+        queryset=Contenido.objects.all(),
+        source='contenido',
+        write_only=True
     )
 
     class Meta:
