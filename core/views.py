d587258a28781ccb09f939bffd9353ca4d83882c
Almonacid98 <gabriel98almonacid@hotmail.com>
2026-05-12 16:15:55 -0300

    Se agregaron permisos de creador reparando los roles de usuario común o el que visualiza y el que crea tipo creador

--- core/views.py
+++ core/views.py
@@ -1,21 +1,40 @@
-from django.shortcuts import render
 from rest_framework import viewsets, filters
-from .models import Usuario, Contenido, Visualizacion
-from .serializers import UsuarioSerializer, ContenidoSerializer, VisualizacionSerializer
-
-
-class UsuarioViewSet(viewsets.ModelViewSet):
-    queryset = Usuario.objects.all()
-    serializer_class = UsuarioSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['nombre', 'email']
-
+from django.db.models import Count
+from .models import Contenido, Visualizacion
+from .serializers import ContenidoSerializer, VisualizacionSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 
 class ContenidoViewSet(viewsets.ModelViewSet):
     queryset = Contenido.objects.all()
     serializer_class = ContenidoSerializer
+    permission_classes = [IsAuthenticated]
+
     filter_backends = [filters.SearchFilter]
     search_fields = ['titulo', 'genero', 'tipo']
+
+    def perform_create(self, serializer):
+
+        if self.request.user.rol != 'creador':
+            raise PermissionDenied(
+                "Solo los creadores pueden subir contenidos."
+            )
+
+        serializer.save(creador=self.request.user)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        genero = self.request.query_params.get('genero')
+        if genero:
+            queryset = queryset.filter(genero=genero)
+
+        if self.request.query_params.get('top') == 'true':
+            queryset = queryset.annotate(
+                total_vistas=Count('visualizacion')
+            ).order_by('-total_vistas')
+
+        return queryset
 
 
 class VisualizacionViewSet(viewsets.ModelViewSet):
