2ae54e321810da079e36af4d570e9cd74110c3b6
Almonacid98 <gabriel98almonacid@hotmail.com>
2026-05-12 16:13:08 -0300

    se modificaron los modelos para actuar independientes del user, además de quitarlo

--- core/models.py
+++ core/models.py
@@ -1,22 +1,5 @@
 from django.db import models
-
-class Usuario(models.Model):
-    ROL_CHOICES = (
-        ('usuario', 'Usuario'),
-        ('creador', 'Creador'),
-    )
-
-    nombre = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
-    edad = models.IntegerField()
-    genero = models.CharField(max_length=50)
-
-    rol = models.CharField(max_length=10, choices=ROL_CHOICES)
-    fecha_registro = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.nombre} ({self.rol})"
+from django.conf import settings
 
 
 class Contenido(models.Model):
@@ -26,15 +9,26 @@ class Contenido(models.Model):
     anio = models.IntegerField()
     duracion_min = models.IntegerField()
 
-    creador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    creador = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.titulo
 
 
 class Visualizacion(models.Model):
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    contenido = models.ForeignKey(Contenido, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    contenido = models.ForeignKey(
+        Contenido,
+        on_delete=models.CASCADE
+    )
+
     fecha_visualizacion = models.DateField(auto_now_add=True)
 
     def __str__(self):
