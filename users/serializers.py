675323663699ffa1e1b904b717d6ab40d3b42863
Almonacid98 <gabriel98almonacid@hotmail.com>
2026-05-26 20:27:47 -0300

    Agregué serializers para registro y perfil de usuarios con hasheo de contraseña y roles por defecto

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,52 @@
 from rest_framework import serializers
 from .models import User
 
-
+# SERIALIZER GENERAL
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
         fields = '__all__'
+
+# REGISTRO DE USUARIOS
+class RegisterSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+
+        fields = (
+            'id',
+            'username',
+            'email',
+            'password',
+            'edad',
+            'genero',
+        )
+
+    def create(self, validated_data):
+
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        # Hashea contraseña
+        user.set_password(password)
+        # Rol por defecto
+        user.rol = 'usuario'
+        user.save()
+        return user
+
+# PERFIL DEL USUARIO
+class ProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'email',
+            'edad',
+            'genero',
+            'rol',
+            'fecha_creacion',
+        )
