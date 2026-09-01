9aa565dd69c5ba9ff082d116bded5f55053c14de
Almonacid98 <gabriel98almonacid@hotmail.com>
2026-09-01 19:54:55 -0300

    Configuración de rutas en App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,64 @@
+import {
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+
 import Home from './views/Home'
+import Login from './views/Login'
+import Register from './views/Register'
+
+import ProtectedRoute from './components/ProtectedRoute'
+
 
 function App() {
-  return <Home />
+
+  return (
+
+    <Routes>
+
+      {/* LOGIN */}
+      <Route
+        path="/login"
+        element={<Login />}
+      />
+
+
+      {/* REGISTRO */}
+      <Route
+        path="/register"
+        element={<Register />}
+      />
+
+
+      {/* HOME PROTEGIDA */}
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+
+            <Home />
+
+          </ProtectedRoute>
+        }
+      />
+
+
+      {/* CUALQUIER OTRA URL */}
+      <Route
+        path="*"
+        element={
+          <Navigate
+            to="/"
+            replace
+          />
+        }
+      />
+
+    </Routes>
+
+  )
 }
+
 
 export default App
