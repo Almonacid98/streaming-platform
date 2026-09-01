7085a7c1b548c0c2e56abcccc04ea4c29e75762b
Almonacid98 <gabriel98almonacid@hotmail.com>
2026-09-01 19:52:10 -0300

    Incorporé AuthProvider a main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './index.css'
+import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
+
+
+createRoot(
+  document.getElementById('root')
+).render(
+
   <StrictMode>
-    <App />
-  </StrictMode>,
+
+    <BrowserRouter>
+
+      <AuthProvider>
+
+        <App />
+
+      </AuthProvider>
+
+    </BrowserRouter>
+
+  </StrictMode>
+
 )
